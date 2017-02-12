6f5483a7d093bd19dd5c691b38d60fa3a81045b4
石楠 <emulephpwyj@gmail.com>
2017-02-12 21:29:19 +0800

    update

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 ## Optimization of index.html
 
- I use grunt to compress the css and javascript files, and inline the style.css to reduce the critical rendering resource number;
+- I use grunt to compress the css and javascript files, and inline the style.css to reduce the critical rendering resource number;
+- I add the async attribute to the script tag, so that it won't delay the rendering; 
+- I add media query to the print.css so it won't prevent delay the rendering;
+- I load js file async, so it is loading and invoke after the whole dom content loaded;
+- I compress the images use grunt, reduce the file size, and it would reduce the critical rendering bytes;
 
- I add the async attribute to the script tag, so that it won't delay the rendering; 
+## Optimization of view/js/main.js
 
- I add media query to the print.css so it won't prevent delay the rendering;
+- I added the translateZ (0) to the .mover element to make it render alone;
+- I use a variable to hold the body.scrollTop to prevent the forced sync layout;
+- Set the #randomPizzas element to flexbox, set flex-wrap to wrap, remove the floating style of the .randomPizzaContainer element, use the elastic box model to optimize the layout;
+- Modify the resizePizzas function, remove the code to trigger the mandatory synchronization layout;
 
- I move the inline script to the bottom of the page, so it is excuting after the whole dom content loaded;
+##run note
 
- I compress the images use grunt, reduce the file size, and it would reduce the critical rendering bytes;
+Open https://xu33.github.io/ to view the website.
+
+##use grunt to build assets
+
+Open ternimal in the project root dir, type grunt, then press enter.
--- views/js/main.js
+++ views/js/main.js
@@ -399,6 +399,7 @@ var pizzaElementGenerator = function(i) {
 };
 
 // resizePizzas(size) is called when the slider in the "Our Pizzas" section of the website moves.
+var pizzaSize = document.querySelector("#pizzaSize");
 var resizePizzas = function(size) {
   window.performance.mark("mark_start_resize");   // User Timing API function
 
@@ -406,13 +407,13 @@ var resizePizzas = function(size) {
   function changeSliderLabel(size) {
     switch(size) {
       case "1":
-        document.querySelector("#pizzaSize").innerHTML = "Small";
+        pizzaSize.innerHTML = "Small";
         return;
       case "2":
-        document.querySelector("#pizzaSize").innerHTML = "Medium";
+        pizzaSize.innerHTML = "Medium";
         return;
       case "3":
-        document.querySelector("#pizzaSize").innerHTML = "Large";
+        pizzaSize.innerHTML = "Large";
         return;
       default:
         console.log("bug in changeSliderLabel");
@@ -421,38 +422,22 @@ var resizePizzas = function(size) {
 
   changeSliderLabel(size);
 
-   // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
-  function determineDx (elem, size) {
-    var oldWidth = elem.offsetWidth;
-    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
-    var oldSize = oldWidth / windowWidth;
-
-    // Changes the slider value to a percent width
-    function sizeSwitcher (size) {
-      switch(size) {
-        case "1":
-          return 0.25;
-        case "2":
-          return 0.3333;
-        case "3":
-          return 0.5;
-        default:
-          console.log("bug in sizeSwitcher");
-      }
+  function widthBySize(size) {
+    switch (size) {
+      case 1: 
+        return '33.3%';
+      case 2: 
+        return '50%';
+      case 3: 
+        return '100%';
     }
-
-    var newSize = sizeSwitcher(size);
-    var dx = (newSize - oldSize) * windowWidth;
-
-    return dx;
   }
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+      var pizza = document.querySelectorAll(".randomPizzaContainer")[i];
+      pizza.style.width = widthBySize(Number(size));
     }
   }
 
@@ -502,8 +487,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var bodyScrollTop = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((bodyScrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
