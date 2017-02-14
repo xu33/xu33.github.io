220225911f44e2afac9243875c6cad8494c181b5
hehe <emulephpwyj@gmail.com>
2017-02-14 23:13:30 +0800

    update

--- js/app.js
+++ js/app.js
@@ -9,32 +9,117 @@ $(function() {
   var CLIENT_ID = 'KIZEC1UO2L1WZVGXEDMAXOIT1WJZZUGIRK1TZA32NPVSMHDY';
   var CLIENT_SECRET = 'PTTVSVXKZ3DKJSZCC3KZUTHO2TUBHC5O1H0313EMBIYASNGT';
   var FOUR_SQUARE_SERACH = 'https://api.foursquare.com/v2/venues/search?';
-
-  var url = FOUR_SQUARE_SERACH + $.param({
+  var DEFAULT_QUERY = '武昌鱼';
+  var QUERY_STRING = {
     ll: '30.5931,114.3054', // wuhan
     client_id: CLIENT_ID,
     client_secret: CLIENT_SECRET,
     v: Date.now(),
-    search: '咖啡',
     limit: 5
-  });
+  };
 
-  $.getJSON(url)
-    .done(function(data) {
-      var venues = data.response.venues;
-      var marker
-      var myLatLng
-      venues.forEach(veune => {
-        myLatLng = {
-          lat: veune.location.lat,
-          lng: veune.location.lng
-        };
+  var throttle = function(callback) {
+    var timer;
 
-        marker = new google.maps.Marker({
-          position: myLatLng,
-          map: map,
-          title: veune.name
+    return function(arg) {
+      if (timer) {
+        clearTimeout(timer);
+      }
+
+      timer = setTimeout(function() {
+        timer = null;
+        callback(arg);
+      }, 500);
+    }
+  };
+
+  var App = {
+    markers: [],
+    init: function() {
+      var url = FOUR_SQUARE_SERACH + $.param($.extend(QUERY_STRING, {
+          query: DEFAULT_QUERY
+        }));
+
+      $.getJSON(url)
+        .done(function(data) {
+          var venues = data.response.venues;
+
+          App.initList(venues);
+          App.updateMap(venues);
         });
+    },
+    initList: function(venues) {
+      var VenuesViewModel = function() {
+        this.keyword = ko.observable('');
+        this.venues = ko.observable(venues);
+        this.showInfoOnMap = function(item) {
+
+        };
+      };
+
+      var vm = new VenuesViewModel();
+
+      // listen to user input change
+      vm.keyword.subscribe(throttle(function(value) {
+        var url = FOUR_SQUARE_SERACH + $.param( $.extend(QUERY_STRING, { query: value, limit: 10 }) );
+
+        $.getJSON(url).done(function(data) {
+          var venues = data.response.venues;
+          vm.venues(venues);
+
+          App.clearMarkers();
+          App.updateMap(venues);
+        })
+      }));
+
+      ko.applyBindings(vm);
+    },
+    clearMarkers: function() {
+      this.markers.forEach(function(marker) {
+        marker.setMap(null);
       });
-    });
+
+      this.markers.length = 0;
+    },
+    clickMarkShowInfowindow: function(marker, infowindow) {
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+    },
+    addMarker: function(veune) {
+      var myLatLng = {
+        lat: veune.location.lat,
+        lng: veune.location.lng
+      };
+
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: veune.name,
+        animation: google.maps.Animation.DROP
+      });
+
+      App.markers.push(marker);
+
+      return marker;
+    },
+    addInfoWindow: function(veune) {
+      var content = '<h3>' + veune.name + '</h3>';
+      var infowindow = new google.maps.InfoWindow({
+        content: content
+      });
+
+      return infowindow;
+    },
+    updateMap: function(venues) {
+      venues.forEach(function(veune) {
+        var marker = App.addMarker(veune);
+        var infowindow = App.addInfoWindow(veune);
+
+        App.clickMarkShowInfowindow(marker, infowindow);
+      });
+    }
+  };
+
+  App.init();
 });
