adf33567f13d7a8ca0c2cc3be67ebca9649e81b5
WHUP\99171 <nan.shi@upchina.com>
2017-02-14 18:02:20 +0800

    地图应用

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 /**
  * Created by 99171 on 2017/2/14.
  */
+// 4square doc: https://developer.foursquare.com/docs/venues/search
+// googlemap doc: https://developers.google.com/maps/documentation/javascript/tutorial
+// googlemap api: https://maps.googleapis.com/maps/api/js?key=AIzaSyAW28WaWGvoyW_FMLBilShlwEfrb7grQzM
 
-// 4square: https://developer.foursquare.com/docs/venues/search
-// googlemap: https://maps.googleapis.com/maps/api/js?key=AIzaSyAW28WaWGvoyW_FMLBilShlwEfrb7grQzM
-// https://api.foursquare.com/v2/venues/search?ll=40.7,-74&oauth_token=2ELZE04TLBF5RJDWWHY1TU24T5ICJKX11YJ5A2QBIMLPZFYU&v=20170214
 $(function() {
   var CLIENT_ID = 'KIZEC1UO2L1WZVGXEDMAXOIT1WJZZUGIRK1TZA32NPVSMHDY';
   var CLIENT_SECRET = 'PTTVSVXKZ3DKJSZCC3KZUTHO2TUBHC5O1H0313EMBIYASNGT';
   var FOUR_SQUARE_SERACH = 'https://api.foursquare.com/v2/venues/search?';
+
   var url = FOUR_SQUARE_SERACH + $.param({
-    ll: '30.5931,114.3054',
+    ll: '30.5931,114.3054', // wuhan
     client_id: CLIENT_ID,
     client_secret: CLIENT_SECRET,
     v: Date.now(),
@@ -18,21 +19,22 @@ $(function() {
     limit: 5
   });
 
-  console.log(url);
-
-  /*
-  * ?ll=30.5931,114.3054'
-   + '&client_id=KIZEC1UO2L1WZVGXEDMAXOIT1WJZZUGIRK1TZA32NPVSMHDY'
-   + '&client_secret=PTTVSVXKZ3DKJSZCC3KZUTHO2TUBHC5O1H0313EMBIYASNGT'
-   + '&search=' + escape('热干面')
-  *
-  * */
-
-
-
-
   $.getJSON(url)
     .done(function(data) {
-      console.log(data);
+      var venues = data.response.venues;
+      var marker
+      var myLatLng
+      venues.forEach(veune => {
+        myLatLng = {
+          lat: veune.location.lat,
+          lng: veune.location.lng
+        };
+
+        marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: veune.name
+        });
+      });
     });
 });
