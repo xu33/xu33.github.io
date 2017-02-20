86f13964815747d47533b0ff81a0228fe4607416
hehe <emulephpwyj@gmail.com>
2017-02-20 23:04:32 +0800

    bug fix

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-##run note
+##应用说明
 
-Open https://xu33.github.io/ to view the website.
+使用关键词搜索地图上的你感兴趣的场所
 
-##How to run this website locally
+##关键技术和框架
 
-Double click the index.html to open.
+本应用使用google map api和4square api作为数据来源进行开发，使用knockoutjs作为前端框架，也用到了jquery的部分api。
+
+##如何使用
+
+使用chrome浏览器访问 [https://xu33.github.io](https://xu33.github.io) 即可
--- js/app.js
+++ js/app.js
@@ -6,10 +6,15 @@
 // googlemap api: https://maps.googleapis.com/maps/api/js?key=AIzaSyAW28WaWGvoyW_FMLBilShlwEfrb7grQzM
 
 $(function() {
+  // 4square api's client_id
   var CLIENT_ID = 'KIZEC1UO2L1WZVGXEDMAXOIT1WJZZUGIRK1TZA32NPVSMHDY';
+  // 4square api's client_secret
   var CLIENT_SECRET = 'PTTVSVXKZ3DKJSZCC3KZUTHO2TUBHC5O1H0313EMBIYASNGT';
+  // 4square's search venues api
   var FOUR_SQUARE_SERACH = 'https://api.foursquare.com/v2/venues/search?';
+  // 默认的关键字
   var DEFAULT_QUERY = '武昌鱼';
+  // 默认的查询字符串参数
   var QUERY_STRING = {
     ll: '30.5931,114.3054', // wuhan
     client_id: CLIENT_ID,
@@ -17,7 +22,10 @@ $(function() {
     v: Date.now(),
     limit: 5
   };
+  // 失败提示语
+  var ERROR_MESSAGE = '4SQUARE服务出错了...，请稍后再试';
 
+  // 去抖函数
   var throttle = function(callback) {
     var timer;
 
@@ -30,54 +38,93 @@ $(function() {
         timer = null;
         callback(arg);
       }, 500);
-    }
+    };
+  };
+
+  var loadVenues = function(url) {
+    var dfd = $.Deferred();
+
+    $.ajax({
+      url: url,
+      dataType: 'text'
+    }).done(function(data) {
+      try {
+        data = JSON.parse(data);
+      } catch(e) {
+        alert(ERROR_MESSAGE);
+      }
+
+      if (data.meta.code !== 200) {
+        alert(ERROR_MESSAGE);
+      }
+
+      dfd.resolve(data.response.venues);
+    }).fail(function() {
+      alert(ERROR_MESSAGE);
+      dfd.reject();
+    });
+
+    return dfd;
   };
 
   var App = {
     markers: [],
-    infowindows: [],
+    infoWindows: [],
     init: function() {
+      var self = this;
+
+      // 创建程序的ViewModel
+      var VenuesViewModel = function() {
+        this.keyword = ko.observable('');
+        this.venues = ko.observable([]);
+
+        // 点击列表项的事件监听函数
+        this.showInfoOnMap = function(item) {
+          var id = item.id;
+          var infoWindow = App.infoWindows.find(o => o.id == id);
+          var marker = App.markers.find(o => o.id == id);
+
+          self.hideWindows();
+
+          infoWindow.open(map, marker);
+          map.setCenter(marker.getPosition());
+        };
+      };
+
+      // 实例化ViewModel
+      this.vm = new VenuesViewModel();
+
+      // 监听关键词变化，重新拼装查询字符串，发送搜索请求
+      this.vm.keyword.subscribe(throttle(function(value) {
+        var url = FOUR_SQUARE_SERACH + $.param( $.extend(QUERY_STRING, { query: value, limit: 10 }) );
+        self.request4SquareApi(url);
+      }));
+
+      ko.applyBindings(this.vm);
+
+      // 拼装api地址
       var url = FOUR_SQUARE_SERACH + $.param($.extend(QUERY_STRING, {
           query: DEFAULT_QUERY
         }));
 
-      $.getJSON(url)
-        .done(function(data) {
-          var venues = data.response.venues;
-
-          App.initList(venues);
-          App.updateMap(venues);
-        });
+      // 加载最初的五个地点
+      self.request4SquareApi(url);
     },
-    initList: function(venues) {
-      var VenuesViewModel = function() {
-        this.keyword = ko.observable('');
-        this.venues = ko.observable(venues);
-        this.showInfoOnMap = function(item) {
-          var id = item.id;
-          var infowindow = App.infowindows.find(o => o.id == id);
-          var marker = App.markers.find(o => o.id == id);
-
-          infowindow.open(map, marker);
-        };
-      };
-
-      var vm = new VenuesViewModel();
-
-      // listen to user input change
-      vm.keyword.subscribe(throttle(function(value) {
-        var url = FOUR_SQUARE_SERACH + $.param( $.extend(QUERY_STRING, { query: value, limit: 10 }) );
-
-        $.getJSON(url).done(function(data) {
-          var venues = data.response.venues;
-          vm.venues(venues);
-
+    request4SquareApi: function(url) {
+      var self = this;
+      loadVenues(url).then(function(venues) {
+        if (venues.length > 0) {
+          self.vm.venues(venues);
           App.clearMarkersAndWindows();
           App.updateMap(venues);
-        })
-      }));
-
-      ko.applyBindings(vm);
+        }
+      });
+    },
+    // 隐藏已经打开的infoWindow
+    hideWindows: function() {
+      this.infoWindows.forEach(function(info) {
+        info.close();
+      });
     },
     clearMarkersAndWindows: function() {
       this.markers.forEach(function(marker) {
@@ -85,13 +132,18 @@ $(function() {
       });
 
       this.markers.length = 0;
-      this.infowindows.length = 0;
+      this.infoWindows.length = 0;
     },
-    clickMarkShowInfowindow: function(marker, infowindow) {
+    clickMarkShowInfoWindow: function(marker, infoWindow) {
+      var self = this;
       marker.addListener('click', function() {
-        infowindow.open(map, marker);
+        self.hideWindows();
+
+        infoWindow.open(map, marker);
+        map.setCenter(marker.getPosition());
       });
     },
+    // 新建一个marker实例，并保存到markers数组中
     addMarker: function(veune) {
       var myLatLng = {
         lat: veune.location.lat,
@@ -111,6 +163,7 @@ $(function() {
 
       return marker;
     },
+    // 新建一个infoWindow实例，并保存到infoWindows数组中
     addInfoWindow: function(veune) {
       var content = '<h3>' + veune.name + '</h3>';
       var infowindow = new google.maps.InfoWindow({
@@ -118,22 +171,26 @@ $(function() {
       });
 
       infowindow.id = veune.id;
-      App.infowindows.push(infowindow);
+      App.infoWindows.push(infowindow);
 
       return infowindow;
     },
+    // 根据返回的venues，更新地图上的标记
     updateMap: function(venues) {
+      var lastMarker
+
       venues.forEach(function(veune) {
         var marker = App.addMarker(veune);
-        var infowindow = App.addInfoWindow(veune);
+        var infoWindow = App.addInfoWindow(veune);
 
-        App.clickMarkShowInfowindow(marker, infowindow);
+        App.clickMarkShowInfoWindow(marker, infoWindow);
+
+        lastMarker = marker
       });
 
-      var veune = venues[0];
-      var center = new google.maps.LatLng(veune.location.lat, veune.location.lng);
-
-      map.panTo(center);
+      if (lastMarker) {
+        map.setCenter(lastMarker.getPosition());
+      }
     }
   };
 
